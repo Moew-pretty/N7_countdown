7e14ae1d8cfe459dc5774db35b502639abf2d47c
Moew-pretty <bichngoc08022004@gmail.com>
2025-04-26 03:46:54 +0700

    night

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,14 +41,15 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-//    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
-//    implementation("com.google.firebase:firebase-storage:21.0.1")
-//    implementation("com.google.firebase:firebase-auth")
-//    implementation("com.google.android.gms:play-services-auth:21.3.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+    implementation("com.google.firebase:firebase-storage:21.0.1")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.3.0")
 }
 
 apply (plugin ="com.google.gms.google-services")
